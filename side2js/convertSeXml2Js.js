9e50ad9b28b1f7313f89d49bc129282eaa95270c
Jérôme Bourgeais <bourgeais.jerome@gmail.com>
2014-10-27 13:54:33 +0100

    Adding argv handler Options:  *  --infile, -i  The Selenium IDE XML absolute file path to convert  *  --indir, -d   The absolute dir path containing Selenium IDE XML file to convert  *  --outdir, -o  The absolute dir path storing results out files

--- side2js/convertSeXml2Js.js
+++ side2js/convertSeXml2Js.js
@@ -1,33 +1,73 @@
-GLOBAL.ROOT_AD= __dirname;
-GLOBAL.LOG_LVL= 'debug';
-
-///
-var fs= require('fs');
-
-/// 
-var logger=          require(ROOT_AD + '/libs/logger'),
-    SIde2JS=         require(ROOT_AD + '/libs/sIde2JS'),
-    CasperFormatter= require(ROOT_AD + '/libs/formatters/casper');
+/**
+ *
+ */
 
 
+/// NodeJS modules
+var fs=   require('fs'),
+    path= require('path'),
+    argx= require('yargs'); // argv parser
 
-/// Argv handling
-var regen= false;
-process.argv.forEach(function(val, index, array) {
-    var argx= val.split('=');
-    console.log(argx);
-    switch(argx[0]) {
-        case 'regen':
-            regen= true;
-            break;
+/// SIde2JS modules
+var config=          require('./config.js'),
+    l=               require(ROOT_AD + 'libs/logger'),
+    SIde2JS=         require(ROOT_AD + 'libs/sIde2JS'),
+    CasperFormatter= require(ROOT_AD + 'libs/formatters/casper');
+
+/// Argv def
+var argv= argx.usage("(-i|-I) [options]",{
+        infile: { alias:'i', description:'The Selenium IDE XML absolute file path to convert'},
+        indir:  { alias:'d', description:'The absolute dir path containing Selenium IDE XML file to convert'},
+        outdir: { alias:'o', description:'The absolute dir path storing results out files'},
+        regen:  { alias:'r', help:'Remove existing outfile'}
+    }).argv;
+
+l.info(argv);
+
+/// Argv check
+var outDir= ROOT_AD;
+if(argv.outdir){
+    try{
+        fs.statSync(argv.outdir).isDirectory();
+        outDir= argv.outdir
     }
-});
+    catch (fsE) {
+        l.error('--outdir' + argv.outdir + ' is not a directory');
+        argx.showHelp();
+        process.exit(1);
+    }
+
+}
+
+if(!argv.infile && !argv.indir){
+    argx.showHelp();
+    process.exit(1);
+}
+
+var inFiles= [];
+if(argv.indir){
+    try{
+        var inDirFiles= fs.readdirSync(argv.indir);
+        var inDirXmlFiles= inDirFiles.filter( function(item){ return /\.xml$/i.test(item)});
+        inFiles = inDirXmlFiles.map( function(item){ return argv.indir + '/' + item });
+        
+    }
+    catch (fsE) {
+        l.error('--indir' + argv.indir + ' is not a directory');
+        argx.showHelp();
+        process.exit(1);
+    }
+}
+if(argv.infile){
+    inFiles.push(argv.infile);
+}
 
 
-//var inFilePath =  '/autumn/tests/seIDE/loginFailed.xml';
-//var outFilePath =  '/autumn/tests/seIDE/loginFailed.js';
-var inFilePath =  '/autumn/tests/seIDE/loginSuccessful.xml';
-var outFilePath =  '/autumn/tests/seIDE/loginSuccessful.js';
+/// Convert the given files
 var casperFormatter= new CasperFormatter();
-var side2JS = new SIde2JS(casperFormatter, regen);
-side2JS.makeJSUnitTestFile(inFilePath, outFilePath);
+var side2JS = new SIde2JS(casperFormatter, argv.regen);
+inFiles.forEach( function(inFilePath, ifile){
+    var outFileName= path.basename(inFilePath).replace( /\.xml$/i,'\.js');
+    var outFilePath= outDir + outFileName;
+    side2JS.makeJSUnitTestFile(inFilePath, outFilePath);
+});
