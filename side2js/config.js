e327d84b951cb1ac3912c128f2681c49c9025445
Jérôme Bourgeais <bourgeais.jerome@gmail.com>
2014-10-27 13:50:18 +0100

    Adding cnf file and some globals

---
+++ side2js/config.js
@@ -0,0 +1,12 @@
+GLOBAL.ROOT_AD= __dirname + '/';
+GLOBAL.LOG_LVL= 'debug';
+
+
+var config = {};
+
+config.dev = {};
+config.prod = {};
+config.recette = {};
+
+
+module.exports = config;
--- side2js/libs/logger.js
+++ side2js/libs/logger.js
@@ -1,13 +1,15 @@
 var winston = require('winston');
+var logFileAF = ROOT_AD + 'logs/error.log';
+console.log("Loging in '" + logFileAF + "'");
 
 var l= new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)({ level: LOG_LVL, json: false, timestamp: true }),
-        new winston.transports.File({ level: LOG_LVL, filename: ROOT_AD + '/logs/error.log', json: false })
+        //new (winston.transports.Console)({ level: LOG_LVL, json: false, timestamp: true }),
+        new winston.transports.File({ level: LOG_LVL, filename: logFileAF, json: false })
     ],
     exceptionHandlers: [
-        new (winston.transports.Console)({ json: false, timestamp: true }),
-        new winston.transports.File({ filename: ROOT_AD + '/logs/error.log', json: false })
+        //new (winston.transports.Console)({ json: false, timestamp: true }),
+        new winston.transports.File({ filename: logFileAF, json: false })
     ],
     exitOnError: false
 });
